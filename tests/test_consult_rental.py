11b9d360e189e74b5c586169fe3906d3836f62a4
Jerome Tissot <jerome.tissot@lamache.org>
2023-04-17 18:11:59 +0200

    US15-Unit tests done

---
+++ tests/test_consult_rental.py
@@ -0,0 +1,16 @@
+
+class TestConsultRental:
+    def test_access_rental_status(self, client):
+        with client.session_transaction() as session:
+            session["username"] = "test@test.fr"
+            session["role"] = "admin"
+        response = client.get("/locations", follow_redirects=True)
+        assert response.status_code == 200
+
+    def test_access_rental_data(self, client):
+        with client.session_transaction() as session:
+            session["username"] = "test@test.fr"
+            session["role"] = "admin"
+        response = client.get("/locataires", follow_redirects=True)
+        assert '<title>CoopImmoGestion-Locations</title>' in response.data.decode('utf-8')
+
